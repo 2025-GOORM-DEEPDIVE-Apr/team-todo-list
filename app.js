0a335479523cd41c9a69fc8bf55d908fbca4955f
한상희 <tkdgml822@gmail.com>
2025-04-24 11:26:17 +0900

    B

---
+++ app.js
@@ -0,0 +1,20 @@
+export const App = () => {
+  let a = 0;
+  let b = 0;
+
+  a += 1;
+  b += 2;
+
+  const plus = a + b;
+  const sub = a - b;
+  const div = a / b;
+  const mult = a * b;
+
+  console.log("더하기: " + plus);
+  console.log("빼기: " + plus);
+  console.log("나누기: " + plus);
+  console.log("곱하기: " + plus);
+
+  const now = new Date();
+  console.log(`현재 시각은 ${now.toLocaleString()} 입니다.`);
+};
